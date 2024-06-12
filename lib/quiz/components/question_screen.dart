79b0f3129c552ff2ea85ef33f27d2df7886ed9b3
Remco Volkers <90606491+remcovolkers@users.noreply.github.com>
2024-06-12 09:52:56 +0200

    yuhhhh

--- lib/quiz/components/question_screen.dart
+++ lib/quiz/components/question_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_udemy/quiz/components/answer_button.dart';
+import 'package:flutter_udemy/quiz/data/quiz_questions.dart';
+import 'package:flutter_udemy/quiz/models/quiz_question.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -8,12 +12,44 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  int currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Text('hallo'),
-      ]),
+    final QuizQuestion currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.golosText(
+                color: const Color.fromARGB(255, 177, 211, 255),
+                fontSize: 36,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answer,
+                onTap: answerQuestion,
+              );
+            }),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/quiz/components/quiz.dart
+++ lib/quiz/components/quiz.dart
@@ -2,27 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_udemy/dice-roller/components/gradient_container.dart';
 import 'package:flutter_udemy/quiz/components/question_screen.dart';
 import 'package:flutter_udemy/quiz/components/start_screen.dart';
-import 'package:flutter_udemy/quiz/model/question.dart';
 
 class Quiz extends StatefulWidget {
-  static const List<Question> questions = [
-    Question(
-      question: 'What are the main building blocks of Flutter UIs?',
-      options: ['Functions', 'Components', 'Widgets', 'Blocks'],
-      answer: 'Widgets',
-    ),
-    Question(
-      question: 'Best',
-      options: ['best1', 'best2', 'best3', 'best4'],
-      answer: 'best1',
-    ),
-    Question(
-      question: 'Kest',
-      options: ['kest1', 'kest2', 'kest3', 'kest4'],
-      answer: 'kest4',
-    )
-  ];
-
   const Quiz({super.key});
 
   @override
@@ -40,6 +21,12 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(BuildContext context) {
+    Widget currentScreen = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      currentScreen = const QuestionScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: GradientContainer(
@@ -47,8 +34,7 @@ class _QuizState extends State<Quiz> {
             Color.fromARGB(255, 66, 14, 208),
             Color.fromARGB(255, 29, 9, 84),
           ],
-          child:
-              activeScreen == 'start-screen' ? StartScreen(switchScreen) : const QuestionScreen(),
+          child: currentScreen,
         ),
       ),
     );
