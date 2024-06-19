d4c376244f48e3bb6ccd39664ea4aec55817fe12
Remco Volkers <remco.volkers1@gmail.com>
2024-06-19 18:26:29 +0200

    eyo wassup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_udemy/expense-tracker/expense_tracker.dart';
-import 'package:flutter_udemy/expense-tracker/theming/theme.dart';
+import 'package:flutter_udemy/meals-app/meals_app.dart';
 
 /// Quiz Runner
 
@@ -9,13 +8,27 @@ import 'package:flutter_udemy/expense-tracker/theming/theme.dart';
 // }
 
 /// Expense Tracker runner
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       themeMode: ThemeMode.dark,
+//       darkTheme: darkTheme,
+//       theme: theme,
+//       home: ExpenseTracker(),
+//     ),
+//   );
+// }
+
+/// Meals App runner
 void main() {
   runApp(
     MaterialApp(
-      themeMode: ThemeMode.dark,
-      darkTheme: darkTheme,
-      theme: theme,
-      home: ExpenseTracker(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Meals App'),
+        ),
+        body: const MealsApp(),
+      ),
     ),
   );
 }
---
+++ lib/meals-app/meals_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MealsApp extends StatefulWidget {
+  const MealsApp({super.key});
+
+  @override
+  State<MealsApp> createState() => _MealsAppState();
+}
+
+class _MealsAppState extends State<MealsApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
